ac9287e2d08cba197a255bf9cd0440c730585497
xiaokai666666 <2075276808@qq.com>
2021-02-21 16:01:26 +0800

    更新Readme.md + 添加注释

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # KotWeather
-小而美天气预报App
+这是一款小而美的天气预报Android App，具备如下特色：
+- AndroidX 和 Material Design2的全面使用
+- 使用Kotlin开发，采用MVVM架构设计，使用到Jetpack库中多个重要组件，包括Lifecycle、ViewModel、LiveData、DataBinding、Room
+- 采用单Activity多Fragment设计，其中使用Navigation完成页面的跳转
+- 使用彩云天气Api获取天气数据
+- 使用高德地图SDK进行定位并得以加载定位地点天气情况
--- app/src/main/java/com/example/kotweather/common/RequestExt.kt
+++ app/src/main/java/com/example/kotweather/common/RequestExt.kt
@@ -9,7 +9,11 @@ import com.example.kotweather.common.state.State
 import com.example.kotweather.network.NetExceptionHandle
 import kotlinx.coroutines.launch
 
-
+/**
+ * 这里使用ViewModelScope的原因：
+ * 当在ViewModel中引入协程，如果直接使用CoroutineScope，那么需要手动在onClear方法中取消协程，如果忘记取消协程会导致内存泄漏
+ * 此时需要使用ViewModel扩展属性ViewModelScope来实现协程作用域
+ */
 fun <T: BaseRepository> BaseViewModel<T>.initiateRequest(
         block: suspend () -> Unit,
         loadState: MutableLiveData<State>
